b7bb8f4a7f8b99ebd9df610c60381e6e1a30cd5f
HNathire <143124025+HNathire@users.noreply.github.com>
2024-08-15 12:39:17 +0400

    Final

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
 import os
 
-hiddenimports = ['firebase_admin']
-
 def main():
     app = QApplication(sys.argv)
     gui = Gui()
